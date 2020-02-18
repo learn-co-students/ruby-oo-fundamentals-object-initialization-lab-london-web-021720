a0a9df75a1233c679b39d0a2513a81c70d04fd4f
nickzaf95 <nicholaszafiriou@gmail.com>
2020-02-18 12:02:25 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog
+    #Dog class
+    def initialize(name, breed="Mutt")
+        @name = name
+        @breed = breed
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,7 @@
+class Person
+    #A Person class
+    def initialize(name)
+        @name = name
+    end
+end
+
